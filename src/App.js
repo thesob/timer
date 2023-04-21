a0ef21329fd6a4a6ee4247a1ea7ebbdcc3ba43b4
thesob <patricio.sobradogmail.com>
2023-04-21 14:57:49 -0400

    Hide set in accordion

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useRef, useEffect } from 'react';
 import { useInterval } from '@mantine/hooks';
-import { Stack, Button, TextInput, Title, Text, Flex } from '@mantine/core';
+import { Stack, Button, TextInput, Title, Text, Flex, Accordion } from '@mantine/core';
 import { useForm } from '@mantine/form'
 
 const App = () => {
   const [seconds, setSeconds] = useState(0)
   const inputRef = useRef(null)
+  const [project, setProject] = useState('Timer')
   useEffect(() => {
-    document.title = `Timer - ${interval.active ? 'Running' : 'Stopped'} | ${toTimeString(seconds)}`
+    document.title = `${project} - ${interval.active ? 'Running' : 'Stopped'} | ${toTimeString(seconds)}`
   })
   const padify = (number) => String(number).padStart(2, '0')
   const toTimeString = (s) => `${padify(~~(s / 3600))}:${padify(~~((s % 3600) / 60))}:${padify(s % 60)}`
@@ -47,8 +48,16 @@ const App = () => {
 
   return (
     <Stack align="center">
-      <img src={logo} className="App-logo" alt="logo" />
-      <Text fz='lg' c='dimmed'>Elapsed time</Text>
+      <TextInput
+        //label='Project'
+        radius="lg"
+        size="xl"
+        variant="filled"
+        styles={{ input: { textAlign: 'center', color: 'gray' }, root: { textAlign: 'center', marginTop: 20 }, label: { color: 'gray' } }}
+        disabled={interval.active}
+        onChange={e => setProject(e.currentTarget.value)} value={project}
+      />
+      <img src={logo} className="App-logo" alt="logo" width={350}/>
       <Title order={1} c='darkgray'>{toTimeString(seconds)}</Title>
       <Button
         variant="outline"
@@ -60,40 +69,43 @@ const App = () => {
       >
         {interval.active ? 'Stop' : 'Start'} counting
       </Button>
-      <form>
-        <Flex gap='md'>
-          <TextInput
-            ref={inputRef}
-            name='duration'
-            value={toTimeString(seconds)}
-            //description={toTimeString(seconds)}
-            label='Set elapsed time'
-            radius="lg"
-            size="l"
-            variant="filled"
-            styles={{ input: { textAlign: 'center', color: 'gray' }, root: { textAlign: 'center' }, label: { color: 'gray' } }}
-            disabled={interval.active}
-            onChange={e => setSeconds(toTimeString(e.target.value))}
-            {...form.getInputProps('duration')}
-          />
-          <Button
-            variant="light"
-            radius="md"
-            size="s"
-            color='gray'
-            onClick={() => setSeconds(toSecondsFromDuration(inputRef.current.value))}
-            disabled={!form.isValid() || interval.active}
-            compact
-          >
-            Set
-          </Button>
-        </Flex>
-        <ul style={interval.active ? { color: 'lightgray' } : { color: 'gray' }}>
-          <li>Format is hours:mm:ss</li>
-          <li>Hours can be unlimited</li>
-          <li>Min and sec less than 60</li>
-        </ul>
-      </form>
+      <Accordion variant="default" radius="md" defaultValue="customization">
+        <Accordion.Item value="customization">
+          <Accordion.Control>Set start time</Accordion.Control>
+          <Accordion.Panel>
+            <Flex gap='md'>
+              <TextInput
+                ref={inputRef}
+                name='duration'
+                value={toTimeString(seconds)}
+                radius="lg"
+                size="l"
+                variant="filled"
+                styles={{ input: { textAlign: 'center', color: 'gray' }, root: { textAlign: 'center' }, label: { color: 'gray' } }}
+                disabled={interval.active}
+                onChange={e => setSeconds(toTimeString(e.target.value))}
+                {...form.getInputProps('duration')}
+              />
+              <Button
+                variant="light"
+                radius="md"
+                size="s"
+                color='gray'
+                onClick={() => setSeconds(toSecondsFromDuration(inputRef.current.value))}
+                disabled={!form.isValid() || interval.active}
+                compact
+              >
+                Set
+              </Button>
+            </Flex>
+            <ul style={interval.active ? { color: 'lightgray' } : { color: 'gray' }}>
+              <li>Format is hours:mm:ss</li>
+              <li>Hours can be unlimited</li>
+              <li>Min and sec less than 60</li>
+            </ul>
+          </Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
     </Stack>
   );
 }
