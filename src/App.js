ed59ea28e5ee9f807a30228bd17cfb2c04711d8b
thesob <patricio.sobradogmail.com>
2023-05-17 11:03:30 -0400

    Add session storage of seconds and project name

--- src/App.js
+++ src/App.js
@@ -5,18 +5,49 @@ import { useInterval } from '@mantine/hooks';
 import { Stack, Button, TextInput, Title, Flex, Accordion, Text, Footer } from '@mantine/core';
 import { useForm } from '@mantine/form'
 
+export const SESSION_PROJECT_NAME = 'pname'
+export const SESSION_COUNT = 'count'
+
 const App = () => {
   const [seconds, setSeconds] = useState(0)
   const inputRef = useRef(null)
   const [project, setProject] = useState('Timer')
+
   useEffect(() => {
     document.title = `${project} - ${interval.active ? 'Running' : 'Stopped'} | ${toTimeString(seconds)}`
   })
+
+  useEffect(() => {
+    const sName = window.sessionStorage.getItem(SESSION_PROJECT_NAME)
+    const sCount = window.sessionStorage.getItem(SESSION_COUNT)
+    sName && setProject(sName)
+    sCount && setSeconds(Number.parseInt(sCount))
+  }, [])
+
+  useEffect(() => {
+    window.sessionStorage.setItem(SESSION_COUNT, seconds)
+  }, [seconds])
+
+  const handleIntervalTick = () => {
+    setSeconds((s) => s + 1)
+  }
+
+  const handleClick = () => {
+    const newCountingState = !interval.active
+    document.documentElement.style.setProperty('--logo-animation-state', `${newCountingState ? 'running' : 'paused'}`);
+    interval.toggle()
+  }
+
+  const handleChange = (e) => {
+    const value = e.currentTarget.value
+    setProject(value)
+    window.sessionStorage.setItem(SESSION_PROJECT_NAME, value)
+  }
+
   const padify = (number) => String(number).padStart(2, '0')
   const toTimeString = (s) => `${padify(~~(s / 3600))}:${padify(~~((s % 3600) / 60))}:${padify(s % 60)}`
 
-  const handleIntervalTick = () => setSeconds((s) => s + 1)
-  const interval = useInterval(handleIntervalTick, 1000)
+   const interval = useInterval(handleIntervalTick, 1000)
 
   /** Calculates total seconds from a timestamp shaped "HH:mm:ss"
    *  where seconds and minutes need to be less than 60
@@ -38,23 +69,15 @@ const App = () => {
     }
   })
 
-  const handleClick = () => {
-    const newCountingState = !interval.active
-    //if (newCountingState) { setSeconds(toSecondsFromDuration(inputRef.current.value)) }
-    document.documentElement.style.setProperty('--logo-animation-state', `${newCountingState ? 'running' : 'paused'}`);
-    interval.toggle()
-  }
-
-
   return (
     <Stack align="center">
       <TextInput
         radius="lg"
         size="xl"
-        //variant="filled"
         styles={{ input: { textAlign: 'center', color: 'gray' }, root: { textAlign: 'center', marginTop: 20 }, label: { color: 'gray' } }}
         disabled={interval.active}
-        onChange={e => setProject(e.currentTarget.value)} value={project}
+        onChange={handleChange}
+        value={project}
         />
       <img src={logo} className="App-logo" alt="logo" width={350}/>
       <Title order={1} c='darkgray'>{toTimeString(seconds)}</Title>
