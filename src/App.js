dd65b235123cfd5f6e5cff450821d60825536ca3
Patricio Sobrado <patricio.sobrado@gmail.com>
2024-06-02 19:06:43 -0400

    Make project runs exclusive but allow own task to continue

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
-import './App.css'
-import { Flex, Stack} from '@mantine/core'
-import Stopwatch from './components/Stopwatch'
-import logo from './images/PS_logo.png'
+import "./App.css";
+import { Anchor, Flex, Stack } from "@mantine/core";
+import Stopwatch from "./components/Stopwatch";
+import logo from "./images/PS_logo.png";
 
 const App = () => {
+  const items = [
+    {
+      defaultName: 'Project X',
+      id: 1, 
+      clockVisible: true, 
+      tasks:[ 
+        {defaultName: 'Task X1', id: 1_1, parent:1, clockVisible: false},
+        {defaultName: 'Task X2', id: 1_2, parent:1, clockVisible: false},
+      ]
+    },
+    {
+      defaultName: 'Project Y', 
+      id: 2, 
+      clockVisible: true, 
+      tasks:[]
+    },
+    {
+      defaultName: 'Project Z', 
+      id: 3, 
+      clockVisible: true, 
+      tasks:[]
+    },
+  ]
 
   return (
-    <Stack align='center' justify='center'>
+    <Stack align="center" justify="center">
       <img src={logo} className="App-logo" alt="logo" />
-      <Flex direction={'row'}>
-        <Stopwatch defaultName='Project X' id={1}/>
+      <Flex direction={"row"} gap={'md'}>
+        {
+          items.map((project) => {
+            return (
+              <Stack key={project.id} align="center">
+                <Stopwatch defaultName={project.defaultName} id={project.id} clockVisible={project.clockVisible}/>
+                {
+                  project.tasks.map((task) => 
+                    <Stopwatch 
+                      key={task.id} 
+                      defaultName={task.defaultName} 
+                      id={task.id} 
+                      parentId={task.parent} 
+                      clockVisible={task.clockVisible}
+                    />)
+                }
+              </Stack>
+            )
+        })}
       </Flex>
-      <footer>
-        <a href='https://github.com/thesob/timer' target='blank' rel='external'>Github</a>
-      </footer>
+      <Anchor href="https://github.com/thesob/timer" target="blank" rel="external">
+        Github
+      </Anchor>
     </Stack>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -18,8 +18,9 @@ import {
 } from "../utils/utils";
 import { FaCreativeCommonsZero, FaPlay, FaStop } from "react-icons/fa6";
 import { IoEye, IoEyeOff, IoSettingsSharp } from "react-icons/io5";
+import Emitter from "../utils/Emitter";
 
-const Stopwatch = ({ defaultName, id, clockVisible=true }) => {
+const Stopwatch = ({ defaultName, id, parentId, clockVisible=true }) => {
   const [seconds, setSeconds] = useState(0);
   const [projectName, setProjectName] = useState(defaultName);
   const inputRef = useRef(null);
@@ -30,31 +31,53 @@ const Stopwatch = ({ defaultName, id, clockVisible=true }) => {
   const SESSION_COUNT = addIdTo(SESSION_COUNT_BASE);
   const SESSION_HOURLY_NOTIFICATION = addIdTo(SESSION_HOURLY_NOTIFICATION_BASE);
   const SESSION_PROJECT_NAME = addIdTo(SESSION_PROJECT_NAME_BASE);
+  
+  const handleIntervalTick = () => {
+    setSeconds((s) => s + 1);
+  };
+  
+  const interval = useInterval(handleIntervalTick, 1000);
 
   useEffect(() => {
     const sName = window.sessionStorage.getItem(SESSION_PROJECT_NAME);
-    const sCount = window.sessionStorage.getItem(SESSION_COUNT_BASE);
-    const sHourlyNotification =
-      window.sessionStorage.getItem(SESSION_HOURLY_NOTIFICATION) === "true";
+    const sCount = window.sessionStorage.getItem(SESSION_COUNT);
+    const sHourlyNotification = window.sessionStorage.getItem(SESSION_HOURLY_NOTIFICATION) === "true";
     sName && setProjectName(sName);
     sCount && setSeconds(Number.parseInt(sCount));
     sHourlyNotification && setHourlyNotification(sHourlyNotification);
-  }, [SESSION_HOURLY_NOTIFICATION, SESSION_PROJECT_NAME]);
+  }, [SESSION_HOURLY_NOTIFICATION, SESSION_PROJECT_NAME, SESSION_COUNT]);
+
+useEffect(() => {
+  const onStartCounter = (eventData) => {
+    // console.log(id, 'received', eventData)
+    // Stop the clock for running projects
+    if (eventData.willBeActive && eventData.id !== id && eventData.parentId !== id) {
+      interval.stop()
+    }
+    // Start the clock for the parent
+    if (eventData.willBeActive && eventData.parentId === id) {
+      interval.start()
+    }
+    // Stop the clock for the children
+    if (!eventData.willBeActive && eventData.id === parentId) {
+      interval.stop()
+    }
+  }
+  const listener = Emitter.addListener('StartCounter', onStartCounter)
+
+  return () => { listener.remove()}
+  }, [id, interval, parentId])
 
   useEffect(() => {
-    document.title = `${projectName} - ${
-      interval.active ? "🏃" : "✋"
-    } | ${toTimeString(seconds)}`;
+    if (!parentId)
+      document.title = `${projectName} - ${interval.active ? "🏃" : "✋"} | ${toTimeString(seconds)}`;
     window.sessionStorage.setItem(SESSION_COUNT, seconds);
+
     if (!hourlyNotification) return;
     const residual = seconds % 3600;
     if (residual === 0 && seconds > 0) {
       const hours = ~~(seconds / 3600);
-      // console.log('hour elapsed:', hours)
-      const voiceMsg = `${hours} ${
-        hours > 1 ? "hours" : "hour"
-      } elapsed on task ${projectName}`;
-      // console.log(voiceMsg)
+      const voiceMsg = `${hours} ${hours > 1 ? "hours" : "hour"} elapsed on project ${projectName}`;
       const utterance = new SpeechSynthesisUtterance(voiceMsg);
       const voices = speechSynthesis.getVoices();
       utterance.voice = voices[0];
@@ -62,19 +85,18 @@ const Stopwatch = ({ defaultName, id, clockVisible=true }) => {
     }
   });
 
-  const handleIntervalTick = () => {
-    setSeconds((s) => s + 1);
-  };
-
   const handleStartBtnClick = () => {
-    const newCountingState = !interval.active;
-    document.documentElement.style.setProperty(
-      "--logo-animation-state",
-      `${newCountingState ? "running" : "paused"}`
-    );
-    interval.toggle();
-  };
+    const willBeActive = !interval.active;
+    document.documentElement.style.setProperty("--logo-animation-state", `${willBeActive ? "running" : "paused"}`);
 
+    // Send start/stop event to other stopwatches
+    const payload = {willBeActive, id, parentId, created_at: Date()}
+    // console.log('sent', payload)
+    Emitter.emit('StartCounter', payload)
+
+    interval.toggle();
+  }
+  
   const handleProjectNameChange = (e) => {
     const value = e.currentTarget.value;
     setProjectName(value);
@@ -88,7 +110,6 @@ const Stopwatch = ({ defaultName, id, clockVisible=true }) => {
   const handleEyeBtnClick = () => {
     setIsClockVisible(!isClockVisible)
   }
-  const interval = useInterval(handleIntervalTick, 1000);
 
   return (
     <Stack align="center" gap='sm'>
