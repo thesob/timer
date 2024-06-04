c834226044414c4fa18686ece1d110ed179dda7c
Patricio Sobrado <patricio.sobrado@gmail.com>
2024-06-04 17:14:12 -0400

    Clarify UX for setting layout. Update counter on setting

--- src/components/StopwatchSettings.js
+++ src/components/StopwatchSettings.js
@@ -1,12 +1,15 @@
 import {
   ActionIcon,
   Checkbox,
+  Divider,
   Flex,
+  Stack,
   TextInput,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { toSecondsFromDuration, toTimeString } from "../utils/utils";
 import { FaCheck } from "react-icons/fa6";
+import { useEffect } from "react";
 
 const StopwatchSettings = ({
   seconds,
@@ -17,6 +20,7 @@ const StopwatchSettings = ({
   hourlyNotification,
   setHourlyNotification,
 }) => {
+  useEffect(() => {});
   const handleHourlyNotificationChange = (e) => {
     const value = e.currentTarget.checked;
     setHourlyNotification(value);
@@ -29,9 +33,7 @@ const StopwatchSettings = ({
 
   const form = useForm({
     validateInputOnChange: ["duration"],
-    initialValues: {
-      duration: "00:00:00",
-    },
+    initialValues: { duration: toTimeString(seconds) },
     validate: {
       duration: (value) =>
         /^\d+:[0-5]\d:[0-5]\d$/.test(value) ? null : "Must be hours:mm:ss",
@@ -39,40 +41,47 @@ const StopwatchSettings = ({
   });
 
   return (
-    <>
-      <Flex gap="md">
-        <TextInput
-          ref={inputRef}
-          name="duration"
-          value={toTimeString(seconds)}
-          radius="lg"
-          size="md"
-          variant="filled"
-          styles={{
-            input: { textAlign: "center", color: "gray" },
-            root: { textAlign: "center" },
-            label: { color: "gray" },
-          }}
-          disabled={interval.active}
-          //onChange={e => setSeconds(toTimeString(e.target.value))}
-          {...form.getInputProps("duration")}
-        />
-        <ActionIcon
+    <Stack gap="md" align="left">
+      <Stack gap="sm">
+        <Flex gap="md" align="center">
+          <TextInput
+            ref={inputRef}
+            name="duration"
+            // value={toTimeString(seconds)}
+            radius="md"
+            size="md"
+            variant="filled"
+            label="Set time"
+            description="Set time and click check to change start time"
+            styles={{
+              input: { textAlign: "center", color: "gray" },
+              root: { textAlign: "center" },
+              label: { color: "gray" },
+            }}
+            disabled={interval.active}
+            onChange={(e) => setSeconds(toTimeString(e.target.value))}
+            {...form.getInputProps("duration")}
+          />
+          <ActionIcon
             variant="light"
             radius="lg"
             size="xl"
             aria-label="Set"
             onClick={handleSetBtnClick}
             disabled={interval.active}
+          >
+            <FaCheck style={{ width: "70%", height: "70%" }} />
+          </ActionIcon>
+        </Flex>
+        <ul
+          style={interval.active ? { color: "lightgray" } : { color: "gray" }}
         >
-          <FaCheck style={{ width: "70%", height: "70%" }} />
-        </ActionIcon>
-      </Flex>
-      <ul style={interval.active ? { color: "lightgray" } : { color: "gray" }}>
-        <li>Format is hours:mm:ss</li>
-        <li>Hours can be unlimited</li>
-        <li>Min and sec less than 60</li>
-      </ul>
+          <li>Format is hours:mm:ss</li>
+          <li>Hours can be unlimited</li>
+          <li>Min and sec less than 60</li>
+        </ul>
+      </Stack>
+      <Divider />
       <Checkbox
         checked={hourlyNotification}
         label="Hourly sound alert"
@@ -80,7 +89,7 @@ const StopwatchSettings = ({
         styles={{ label: { color: "gray" } }}
         disabled={interval.active}
       />
-    </>
+    </Stack>
   );
 };
 
