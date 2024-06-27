88bb130e9a3bd400a503c27e33f75947504c6bfb
Patricio Sobrado <patricio.sobrado@gmail.com>
2024-06-26 20:03:13 -0400

    Add breakpoints definitions

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ import { MantineProvider } from "@mantine/core";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider
+      withGlobalStyles 
+      withNormalizeCSS 
+      theme={{
+        breakpoints: {
+          xs: '30em',
+          sm: '48em',
+          md: '64em',
+          lg: '74em',
+          xl: '90em',
+        },
+      }}
+    >
       <App />
     </MantineProvider>
   // </StrictMode>
